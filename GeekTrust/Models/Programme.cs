4506f8b5493d510b7d1e6df266362f155a82a828
divakar bhardwaj <dbhardwaj@contecglobal.com>
2023-04-17 11:01:40 +0530

    minor changes

--- GeekTrust/Models/Programme.cs
+++ GeekTrust/Models/Programme.cs
@@ -22,21 +22,21 @@ namespace GeekTrust.Models
     public class Certification : Programme
     {
         private decimal proMembershipDiscout = 0.02m;
-        public Certification() : base("CERTIFICATION", 3000) { }
+        public Certification() : base("CERTIFICATION", CERTIFICATION_COST) { }
         public override decimal ProMemberShipDiscount { get => proMembershipDiscout; }
     }
 
     public class Degree : Programme
     {
         private decimal proMembershipDiscount = 0.03m;
-        public Degree() : base("DEGREE", 5000) { }
+        public Degree() : base("DEGREE", DEGREE_COST) { }
         public override decimal ProMemberShipDiscount { get => proMembershipDiscount; }
     }
 
     public class Diploma : Programme
     {
         private decimal proMembershipDiscount = 0.01m;
-        public Diploma() : base("DIPLOMA", 2500) { }
+        public Diploma() : base("DIPLOMA", DIPLOMA_COST) { }
         public override decimal ProMemberShipDiscount { get => proMembershipDiscount; }
     }
 }
